fba770d054980a9911c459cc64c03f0272774094
yubo <yubo@yubo.org>
2015-07-24 17:37:56 +0800

    Exercise 4. In your attach function, create a virtual memory mapping for the E1000's BAR 0 by calling mmio_map_region
    make qemu-nox and you should get 0x80080783

--- kern/e1000.c
+++ kern/e1000.c
@@ -8,6 +8,11 @@ e1000_attach(struct pci_func *pcif)
 	// Enable PCI device
 	pci_func_enable(pcif);
 
+	// Memory map I/O for PCI device
+	e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+
+	cprintf("E1000 status: %08x You should get 0x80080783\n", e1000[E1000_STATUS]);
+
 	return 0;
 }
 
