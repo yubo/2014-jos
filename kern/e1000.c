3f14df0e055ca120e70e3f202a9642e65e3326f0
yubo <yubo@yubo.org>
2015-07-24 17:21:42 +0800

    Exercise 3. Implement an attach function to initialize the E1000. make grade should now pass the pci attach test

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,13 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+int
+e1000_attach(struct pci_func *pcif)
+{
+	// Enable PCI device
+	pci_func_enable(pcif);
+
+	return 0;
+}
+
